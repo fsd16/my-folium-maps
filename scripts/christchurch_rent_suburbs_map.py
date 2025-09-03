fb4f623a1b2665e1df9799059c0a95889e4f35cb
fdrabsch <finndrabsch@outlook.co.nz>
2025-09-03 14:47:35 +1200

    Source suburbs from notion

--- scripts/christchurch_rent_suburbs_map.py
+++ scripts/christchurch_rent_suburbs_map.py
@@ -1,30 +1,40 @@
 import os
 from pathlib import Path
+from re import sub
 import folium
+import notion
+from notion import query
+
 import requests
 
 BASE_PATH = Path(__file__).parent
 OUTPUT_DIR = BASE_PATH.parent / "public"  # GitHub Pages folder
 OUTPUT_FILE = OUTPUT_DIR / "christchurch_rent_suburbs.html"
 
+NOTION_TOKEN = os.environ.get("NOTION_TOKEN")
+if NOTION_TOKEN is None:
+    raise ValueError("Please set the NOTION_TOKEN environment variable")
+
 LINZ_API_KEY = os.environ.get("LINZ_API_KEY")
 if LINZ_API_KEY is None:
     raise ValueError("Please set the LINZ_API_KEY environment variable")
 
-# Suburbs list (Get this from notion eventually)
-suburbs = [
-    "Wigram",
-    "Upper Riccarton",
-    "Aidanfield",
-    "Hillmorton",
-    "Hoon Hay",
-    "Somerfield",
-    "Spreydon",
-    "Beckenham",
-    "Cashmere",
-    "Halswell",
-    "St Albans",
-]
+suburbs = []
+
+page = notion.Block("24c33ffcade880be9a38eb704e58d526")
+res = page.retrieve_children()["results"]
+inside_suburb_section = False
+for re in res:
+    if re["type"] == "heading_2":
+        text = "".join([t["plain_text"] for t in re["heading_2"]["rich_text"]])
+        if text.strip().lower() == "suburbs":
+            inside_suburb_section = True
+        else:
+            inside_suburb_section = False
+
+    if inside_suburb_section and re["type"] == "heading_3":
+        suburb = re["heading_3"]["rich_text"][-1]["plain_text"]
+        suburbs.append(suburb)
 
 # Build LINZ WFS request
 cql_filter = f"name IN ('{'\',\''.join(suburbs)}')"
